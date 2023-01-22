fbd2d94ed373240262dc9dc3550cc508926611f2
tamurakeito <kit_tamtam@icloud.com>
2023-01-22 22:27:43 +0900

    deploy to github-pages

--- src/pages/traning/index.tsx
+++ src/pages/traning/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Question } from "types/types";
 import { Text } from "atom/text";
-import { Button } from "atom/button";
+// import { Button } from "atom/button";
 import "./index.scss";
 import { useThemaContext } from "component/thema-provider";
 // import { useTrainingContext } from "component/training-provider";
