f53c9bd9db99a6b22724575488b56faba5772e15
tamurakeito <tamurakeito@chibikei-Mac-mini.local>
2024-09-01 17:41:44 +0900

    deploy to github-pages

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -9,25 +9,30 @@ import { useThemaContext } from "component/thema-provider";
 import { useTrainingContext } from "component/training-provider";
 import { useSurfaceContext } from "component/surface-provider";
 
-import HerbalMedicine from "data/herbal-medicine.json";
+// import HerbalMedicine from "data/herbal-medicine.json";
 
-import Hangul from "data/hangul.json";
-import Hangul2 from "data/hangul2.json";
-import FrequentPhrase from "data/frequent-phrases.json";
+// import Hangul from "data/hangul.json";
+// import Hangul2 from "data/hangul2.json";
+// import FrequentPhrase from "data/frequent-phrases.json";
 
-import KokushiYokomoji from "data/kokushi-yokomoji.json";
+// import KokushiYokomoji from "data/kokushi-yokomoji.json";
 // import KokushiEnglish from "data/kokushi-english.json";
-import KokushiSign from "data/kokushi-sign.json";
+// import KokushiSign from "data/kokushi-sign.json";
 // import KokushiDrug from "data/kokushi-drug.json";
-import KokushiDermadrome from "data/kokushi-dermadrome.json";
+// import KokushiDermadrome from "data/kokushi-dermadrome.json";
 // import ResidentAnesthetic from "data/resident-anesthetic.json";
+
+import KoreanWords1 from "data/korean-words1.json";
 
 export const Home = () => {
   const { thema, setThemaContent } = useThemaContext();
   const { setQuestionLength } = useTrainingContext();
   const { setSurface } = useSurfaceContext();
+  const themaList : Array<ThemaContent> = [
+    KoreanWords1
+  ];
   useEffect(() => {
-    setThemaContent(HerbalMedicine);
+    setThemaContent(themaList[0]);
   }, []);
   useEffect(() => {
     document.title = thema.title;
@@ -69,10 +74,6 @@ export const Home = () => {
     setThemaContent(thema);
     document.title = thema.title;
   };
-  const themaList : Array<ThemaContent> = [
-    HerbalMedicine,KokushiYokomoji,KokushiSign,KokushiDermadrome,
-    Hangul,Hangul2,FrequentPhrase
-  ];
   const [themaIndex, setThemaIndex] = useState(0);
   const changeThema = () => {
     setThemaIndex(themaIndex + 1 < themaList.length ? themaIndex + 1 : 0);
