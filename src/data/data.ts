a668463f98348718813c89720edb302755cb9f49
tamurakeito <tamurakeito@chibikei-Mac-mini.local>
2024-10-03 20:35:23 +0900

    Local changes to data.ts

--- src/data/data.ts
+++ src/data/data.ts
@@ -2,9 +2,13 @@ import { ThemaContent } from "types/types";
 import KoreanWords1 from "./json/korean-words1.json";
 import KoreanWords2 from "./json/korean-words2.json";
 import KoreanPhrase from "./json/korean-phrases.json";
+import KpopLyricsBTS1 from "./json/kpop-lyrics-bts1.json";
+import KpopLyricsSEVENTEEN1 from "./json/kpop-lyrics-seventeen1.json";
 
 export const themaList: Array<ThemaContent> = [
   KoreanWords1,
   KoreanWords2,
   KoreanPhrase,
+  KpopLyricsBTS1,
+  KpopLyricsSEVENTEEN1
 ];
