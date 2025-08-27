5c34e2b4203af76b5bdc9c8aa2a4a42355a57b86
tamurakeito <tamurakeito@chibikei-Mac-mini.local>
2025-08-27 14:02:49 +0900

    宅建試験分追加

--- src/data/data.ts
+++ src/data/data.ts
@@ -7,8 +7,22 @@ import KoreanWords3 from "./json/korean-words3.json";
 import KoreanPhrase from "./json/korean-phrases.json";
 import KpopLyricsBTS1 from "./json/kpop-lyrics-bts1.json";
 import KpopLyricsSEVENTEEN1 from "./json/kpop-lyrics-seventeen1.json";
+import TakkenR6 from "./json/takken-r6.json";
+import TakkenR5 from "./json/takken-r5.json";
+import TakkenR4 from "./json/takken-r4.json";
+import TakkenR3_2 from "./json/takken-r3-2.json";
+import TakkenR3_1 from "./json/takken-r3-1.json";
+import TakkenR2_2 from "./json/takken-r2-2.json";
+import TakkenR2_1 from "./json/takken-r2-1.json";
 
 export const themaList: Array<ThemaContent> = [
+  TakkenR6,
+  TakkenR5,
+  TakkenR4,
+  TakkenR3_2,
+  TakkenR3_1,
+  TakkenR2_2,
+  TakkenR2_1,
   BasicPhraseEnglish,
   BasicPhraseKorean,
   KoreanWords1,
