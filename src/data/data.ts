6f9f7be43849411dd475e28e3d906a060d92b94a
tamurakeito <keito.tamura@up-sider.com>
2025-10-19 00:44:39 +0900

    宅建のみ

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,12 +1,4 @@
 import { ThemaContent } from "types/types";
-import BasicPhraseEnglish from "./json/basic-phrase-english.json";
-import BasicPhraseKorean from "./json/basic-phrase-korean.json";
-import KoreanWords1 from "./json/korean-words1.json";
-import KoreanWords2 from "./json/korean-words2.json";
-import KoreanWords3 from "./json/korean-words3.json";
-import KoreanPhrase from "./json/korean-phrases.json";
-import KpopLyricsBTS1 from "./json/kpop-lyrics-bts1.json";
-import KpopLyricsSEVENTEEN1 from "./json/kpop-lyrics-seventeen1.json";
 import TakkenR6 from "./json/takken-r6.json";
 import TakkenR5 from "./json/takken-r5.json";
 import TakkenR4 from "./json/takken-r4.json";
@@ -22,13 +14,5 @@ export const themaList: Array<ThemaContent> = [
   TakkenR3_2,
   TakkenR3_1,
   TakkenR2_2,
-  TakkenR2_1,
-  BasicPhraseEnglish,
-  BasicPhraseKorean,
-  KoreanWords1,
-  KoreanWords2,
-  KoreanWords3,
-  KoreanPhrase,
-  KpopLyricsBTS1,
-  KpopLyricsSEVENTEEN1,
+  TakkenR2_1
 ];
