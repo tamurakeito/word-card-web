2ff970412a3f3fbf26ae4b2cbce557f6609c63c0
tamurakeito <keito.tamura@up-sider.com>
2025-11-08 17:05:13 +0900

    deploy to github-pages

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,18 +1,6 @@
 import { ThemaContent } from "types/types";
-import TakkenR6 from "./json/takken-r6.json";
-import TakkenR5 from "./json/takken-r5.json";
-import TakkenR4 from "./json/takken-r4.json";
-import TakkenR3_2 from "./json/takken-r3-2.json";
-import TakkenR3_1 from "./json/takken-r3-1.json";
-import TakkenR2_2 from "./json/takken-r2-2.json";
-import TakkenR2_1 from "./json/takken-r2-1.json";
+import KesyouhinKentei from "./json/kesyouhin-kentei.json"
 
 export const themaList: Array<ThemaContent> = [
-  TakkenR6,
-  TakkenR5,
-  TakkenR4,
-  TakkenR3_2,
-  TakkenR3_1,
-  TakkenR2_2,
-  TakkenR2_1
+  KesyouhinKentei
 ];
